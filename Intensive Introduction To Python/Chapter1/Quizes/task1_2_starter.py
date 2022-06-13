ffaba6260ccb679061e1a70acdd826b292857788
Jashon Menya <stevemenya14@gmail.com>
2022-06-13 17:37:06 -0500

    day one stuff

---
+++ Intensive Introduction To Python/Chapter1/Quizes/task1_2_starter.py
@@ -0,0 +1,58 @@
+"""
+For this task, you are to manually extract the domain name out of the URLs mentioned on our slide.
+
+
+https://docs.python.org/3/
+https://www.google.com?qws_rd=ssl#q=python
+http://localhost:8005/contact/501
+
+
+"""
+my_data = [
+    'https://docs.python.org/3/',
+    'https://www.google.com?qws_rd=ssl#q=python',
+    'http://localhost:8005/contact/501',
+    'https://jjmenya.testing.com/3/'
+]
+
+
+def get_domain_name(data):
+    suffixes = [':', '/', '?']
+    # loop through the urls coming in
+    for url in data:
+
+        if url.startswith('http://'):
+            get_http_domain(url)
+
+        elif url.startswith('https://'):
+            get_https_domain(url)
+
+
+def get_http_domain(url):
+    # loop through string in the url and remove the left side
+    remaining = url[len('http://'):]
+    # print(remaining)
+
+    # loop and remove things on left
+    position = remaining.find(':')
+    if position != 1:
+        domain = remaining[:position]
+
+    print(domain)
+
+
+def get_https_domain(url):
+    remaining = url[len('https://'):]
+    # print(remaining)
+
+    # loop and remove things on left
+    position = remaining.find('/')
+    if position < 0:
+        position = remaining.find('?')
+    if position != 1:
+        domain = remaining[:position]
+
+    print(domain)
+
+
+get_domain_name(my_data)
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Python
 Repo to hold  all python topics I'm learning
+
+Intensive Introduction to Python: Optum Training
+Chapters at a Glance
+Chapter 1: Python Language Overview
+Chapter 2: File and Flow Control
+Chapter 3: Functions
+Chapter 4: Object-Oriented Python
+Chapter 5: Introduction to Python Standard Library
+Chapter 6: Network Programming
+Chapter 7: Regular Expression within Python
+Course Summary
